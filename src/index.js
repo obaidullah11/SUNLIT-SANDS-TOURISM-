6598a2b6222488513a8da40280706fbf9d0be721
obaidullah11 <muhammadobaidullah1122@gmail.com>
2025-04-15 17:22:25 +0500

    resolve cjhanges

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Set basename conditionally based on environment
+const basename = process.env.NODE_ENV === 'production' ? '/SUNLIT-SANDS-TOURISM-' : '/';
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-
 );
 
 reportWebVitals();
